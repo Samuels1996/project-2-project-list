8f1649cc1b79c1faa38880f84ed8952bfe93d55a
Brian Samuels <samuels.brian9@gmail.com>
2022-10-27 10:20:59 -0500

    hello world!

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,2 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import Projects from './pages/Projects';
 
+export default function ProjectContainer() {
+    const [currentPage, setCurrentPage] = useState('Home');
+
+    const renderPage = () => {
+    return <Projects />;
+        
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+            {handlePageChange}
+            {renderPage()}
+        </div>
+    );
+}
