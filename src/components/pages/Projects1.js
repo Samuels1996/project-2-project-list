7079daa57ec1c27f54969852d2caeb48783cc7a5
Mark Alfano <mpalfano4@gmail.com>
2022-10-30 16:59:35 -0400

    refactoring project and adding image path to json

--- src/components/pages/Projects1.js
+++ src/components/pages/Projects1.js
@@ -1,34 +1,46 @@
 import React from "react";
-import {
-  Carousel,
-  CarouselItem,
-  CarouselControl,
-  CarouselIndicators,
-  CarouselCaption,
-} from "reactstrap";
+import projects from "../../projectData/project1";
 
-import BeerMeal from "../../images/project1/beermeal.png";
-import CampingJournal from "../../images/project1/campingjournal.png";
-import CheckYourMovie from "../../images/project1/checkyourmovie.png";
-import FindFlix from "../../images/project1/findflix.png";
-import FlightPlanner from "../../images/project1/flightplanner.png";
-import LearningMyWay from "../../images/project1/learningmyway.png";
-import Motivation from "../../images/project1/motivation.png";
-import MusicHub from "../../images/project1/musichub.png";
-import NewsHeadline from "../../images/project1/newsheadline.png";
-import SearchDisplayer from "../../images/project1/searchdisplayer.png";
-import WhatsTheWord from "../../images/project1/whatstheword.png";
-import Where2Watch from "../../images/project1/where2watch.png";
-import WordPlay from "../../images/project1/wordplay.png";
-import Ymdb from "../../images/project1/ymdb.png";
+// import BeerMeal from "../../images/project1/beermeal.png";
+// import CampingJournal from "../../images/project1/campingjournal.png";
+// import CheckYourMovie from "../../images/project1/checkyourmovie.png";
+// import FindFlix from "../../images/project1/findflix.png";
+// import FlightPlanner from "../../images/project1/flightplanner.png";
+// import LearningMyWay from "../../images/project1/learningmyway.png";
+// import Motivation from "../../images/project1/motivation.png";
+// import MusicHub from "../../images/project1/musichub.png";
+// import NewsHeadline from "../../images/project1/newsheadline.png";
+// import SearchDisplayer from "../../images/project1/searchdisplayer.png";
+// import WhatsTheWord from "../../images/project1/whatstheword.png";
+// import Where2Watch from "../../images/project1/where2watch.png";
+// import WordPlay from "../../images/project1/wordplay.png";
+// import Ymdb from "../../images/project1/ymdb.png";
 
 export default function Projects1() {
+  console.log(projects);
   return (
     <main>
       {/* <h1 id="title">Hello World!</h1> */}
       <div id="carousel">
         <section className="gallery">
-          <span id="pic1">
+          {projects.map((project, index) => {
+            return (
+              <span id={`pic${index + 1}`} key={project.id}>
+                <img
+                  src="./images/project1/flightplanner.png"
+                  alt={project.title}
+                ></img>
+                <a className="demo" href={project.deployed}>
+                  Live Demo
+                </a>
+                <a className="github" href={project.repo}>
+                  Github
+                </a>
+              </span>
+            );
+          })}
+
+          {/* <span id="pic1">
             <img src={WordPlay} alt="Word Play"></img>
             <a className="demo" href="https://github.com/MpAlfano">
               Live Demo
@@ -39,9 +51,21 @@ export default function Projects1() {
           </span>
           <span id="pic2">
             <img src={Where2Watch} alt="Where 2 Watch"></img>
+            <a className="demo" href="https://github.com/MpAlfano">
+              Live Demo
+            </a>
+            <a className="github" href="https://github.com/MpAlfano">
+              Github
+            </a>
           </span>
           <span id="pic3">
             <img src={BeerMeal} alt="Beer Meal"></img>
+            <a className="demo" href="https://github.com/MpAlfano">
+              Live Demo
+            </a>
+            <a className="github" href="https://github.com/MpAlfano">
+              Github
+            </a>
           </span>
           <span id="pic4">
             <img src={CampingJournal} alt="Camping Journal"></img>
@@ -75,7 +99,7 @@ export default function Projects1() {
           </span>
           <span id="pic14">
             <img src={WhatsTheWord}></img>
-          </span>
+          </span> */}
         </section>
       </div>
     </main>
